eabae45a3939a94964bb6ace12d1465901317592
Victor Sergeyev <victor17011998@yandex.ru>
2018-01-23 15:05:57 +0400

    report really all

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -10,7 +10,7 @@
 session_start();
 
 // DEBUG
-ini_set('error_reporting', E_ALL);
+ini_set('error_reporting', E_ALL | E_NOTICE | E_STRICT | E_DEPRECATED);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 ini_set('html_errors', 1);
