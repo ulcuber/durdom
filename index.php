d0fc3158128c754f19400bec339a4e362f48c653
NecroSpider <35307432+NecroSpider@users.noreply.github.com>
2018-02-05 21:45:27 +0400

    Update index.php

--- index.php
+++ index.php
@@ -24,20 +24,21 @@ $news = store('lastNews', 1);
         <ul class="rslides" id="slider">
             <div class="slid banner1">
                 <div class="caption">
-                    <h3>Заголовок.</h3>
-                    <p>Текст.</p>
+                   <h3>Добро пожаловать в GamersNews.</h3>
+                    <p>GamersNews, с нами интересно.</p>
+                    
                 </div>
             </div>
             <div class="slid banner2">
                 <div class="caption">
-                    <h3>Заголовок.</h3>
-                    <p>Текст.</p>
+                    <h3>Самые лучшие новости.</h3>
+                    <p>Читай только лучшее.</p>
                 </div>
             </div>
             <div class="slid banner3">
                 <div class="caption">
-                    <h3>Заголовок.</h3>
-                    <p>Текст.</p>
+                    <h3>Самые свежие новости.</h3>
+                    <p>Читай новости пока они еще пахнут свежестью.</p>
                 </div>
             </div>
         </ul>
@@ -79,7 +80,7 @@ $news = store('lastNews', 1);
                             <img src="images/v3.jpg" alt="img07"/>
                        </div>
                        <div class="col-md-8 sub-text">
-                                <a href="#">Death Stranding(PS4)</a>
+                                <a href="#">Death Stranding (PS4)</a>
                              <p>Death Stranding – экшн японской студии Kojima Productions, первая игра директора Хидэо Кодзимы и его компании после выхода Kojima Productions из Konami и превращения в независимую студию. Игру анонсировали на конференции Sony на E3 2016. Главных персонажей изобразят актеры Норман Рид и Мэдс Миккельсен – посредством захвата движения, сканирования лица и голоса. Режиссер Гильермо дель Торо станет прообразом другого персонажа посредством сканирования лица и тела. Название игры происходит от феномена выбрасыванием на мель («stranding») китов. Действие игры происходит в открытом мире. Есть мультиплеерные функции. Полномасштабная разработка игры началась в 2017 году.</p>
                        </div>
                         <div class="clearfix"> </div>
@@ -87,6 +88,7 @@ $news = store('lastNews', 1);
                 </div>
                 <div class="col-md-7 trailer">
                  <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLY0KbDiiFYeMUnqOjbB-iz0pkQIdjNjsb" frameborder="0" gesture="media" allowfullscreen></iframe>
+                 <iframe width="560" height="315" src="https://www.youtube.com/embed/lKgaa8wXr7Q" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                 </div>
                  <div class="clearfix"> </div>
             </div>
@@ -101,7 +103,7 @@ foreach ($news as $row) {
      <div class="container">
          <div class="poster-info">
              <h3><?=$row['head']?></h3>
-             <p><?=mb_substr($row['post2'], 0, 40)?></p>
+             <p><?=mb_substr($row['post2'], 0, 80)?></p>
               <a class="hvr-bounce-to-bottom" href="<?=$url?>">Подробнее</a>
          </div>
      </div>
@@ -117,12 +119,13 @@ foreach ($news as $row) {
          <div class="x-box_sec">
              <div class="col-md-7 x-box-left">
                  <h2><?=$row['head']?></h2>
-                <p><?=mb_substr($row['post2'], 0, 40)?></p>
+                <p><?=mb_substr($row['post2'], 0, 80)?></p>
                  <a class="hvr-bounce-to-top" href="<?=$url?>">Подробнее</a>
              </div>
              <div class="col-md-5 x-box-right">
                  <img src="images/xbox.jpg" class="img-responsive" alt=""/>
              </div>
+            
              <div class="clearfix"></div>
          </div>
      </div>
